4d6db1600a3ddc280bc78d5cf8fac8dd10d1fc5f
GaLaQe <loicbokorohirwa@hotmail.be>
2022-03-17 21:41:55 +0100

    lol

--- src/Library.cpp
+++ src/Library.cpp
@@ -2,7 +2,16 @@
 // Created by galaqe on 15/03/22.
 //
 
+#include <sstream>
 #include "tinyxml_2_6_2/tinyxml/tinyxml.h"
 #include "Voertuig.h"
 #include "Baan.h"
 #include "Verkeerslicht.h"
+
+std::string int_to_string(int integer) {
+    std::string new_string;
+    std::ostringstream convert;
+    convert << integer;
+    new_string = convert.str();
+    return new_string;
+}
